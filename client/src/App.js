fc6f1ac37af4b00c824600633b6cd2bff6883e65
Arlan Dantas <arlanjose@gmail.com>
2020-07-25 12:57:33 -0300

    Igreja view

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,14 @@ import {
 import Home from './routes/home';
 import Noticias from './routes/noticias';
 import Noticia from './routes/noticia';
+import Igreja from './routes/igreja';
 
 export default function () {
   return (
     <Router>
       <div id="bkg"></div>
       <Switch>
+        <Route path={["/igreja"]} component={Igreja} />
         <Route path={["/noticias"]} component={Noticias} />
         <Route path={["/noticia/:id"]} component={Noticia} />
         <Route path={['/', "/home"]} component={Home} />
