1994cfc87741a42d6ae809cd1f5c7915ebd1478f
Arlan Dantas <arlanjose@gmail.com>
2020-07-25 13:07:11 -0300

    dist 0.1

---
+++ client/dist/precache-manifest.6937efc9854ca40edc07826d02a16b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041b481ed94614b67d83bfae2f783eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "849d870cead721477919",
+    "url": "/static/css/main.f87ac703.chunk.css"
+  },
+  {
+    "revision": "9f9fd1bf97357a84e1b1",
+    "url": "/static/js/2.90518021.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.90518021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849d870cead721477919",
+    "url": "/static/js/main.f16ba1e8.chunk.js"
+  },
+  {
+    "revision": "97c4f5ff72e586d602a3",
+    "url": "/static/js/runtime-main.58182583.js"
+  }
+]);
