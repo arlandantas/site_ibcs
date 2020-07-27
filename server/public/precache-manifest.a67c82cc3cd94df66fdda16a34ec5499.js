9bf00c6089b3de4a0002117a1525b55cca83906d
Arlan Dantas <arlanjose@gmail.com>
2020-07-27 18:39:41 -0300

    PHP and codeigniter

---
+++ server/public/precache-manifest.a67c82cc3cd94df66fdda16a34ec5499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde04218a83d7d0ca9c2ff73ed6d6cc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d478af283cf0c372f75b",
+    "url": "/static/css/main.d3ab5ef2.chunk.css"
+  },
+  {
+    "revision": "9f9fd1bf97357a84e1b1",
+    "url": "/static/js/2.90518021.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.90518021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d478af283cf0c372f75b",
+    "url": "/static/js/main.60c1161e.chunk.js"
+  },
+  {
+    "revision": "97c4f5ff72e586d602a3",
+    "url": "/static/js/runtime-main.58182583.js"
+  }
+]);
