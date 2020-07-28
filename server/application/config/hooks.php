17a7693e636622c2642c48a6380d230696379291
Arlan Dantas <arlanjose@gmail.com>
2020-07-28 06:54:52 -0300

    removendo pre_system hooks

--- server/application/config/hooks.php
+++ server/application/config/hooks.php
@@ -11,24 +11,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
-$hook['pre_system'] = function() {
-  $handle = @fopen('./.env', "r");
-  if ($handle) {
-    while (($buffer = fgets($handle)) !== false) {
-      $buffer = trim($buffer);
-      if (substr($buffer, 0, 1) != '#' && $buffer != '') {
-        $parts = array_map(function ($e) { return trim($e); }, explode("=", $buffer));
-        if (!isset($env[mb_strtolower($parts[0])])) {
-          // $env[mb_strtolower($parts[0])] = $parts[1];
-          $_ENV[$parts[0]] = isset($parts[1]) ? $parts[1] : TRUE;
-          $_SERVER[$parts[0]] = isset($parts[1]) ? $parts[1] : TRUE;
-        }
-      }
-    }
-    if (!feof($handle)) {
-      echo "Erro: falha inexperada de fgets()\n";
-    }
-  } else {
-    throw new Exception(".env file not found! Crie um arquivo .env em /server/ do projeto se baseando em /server/.env.example!", 1);
-  }
-};
