3a34cb74850270bfc7bb205d8924a4f81858ddc8
Arlan Dantas <arlanjose@gmail.com>
2020-07-27 22:30:21 -0300

    Usando client/dist como pasta pública

--- server/application/controllers/Inicial.php
+++ server/application/controllers/Inicial.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Inicial extends CI_Controller {
 	public function index() {
-		$this->load->helper('file');
-		echo read_file('./public/index.html');
+		$this->load->helper('url');
+		echo file_get_contents(site_url('index.html'));
 	}
 }
