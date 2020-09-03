a644e9b53ff2cc7510bad33898983b6dd5e19590
Arlan Dantas <arlanjose@gmail.com>
2020-09-02 22:37:33 -0300

    Botão Paypal na tela de doações

--- client/src/routes/doacoes.jsx
+++ client/src/routes/doacoes.jsx
@@ -61,6 +61,22 @@ class Doacoes extends React.Component {
               <img src="imgs/qr-picpay-dark.png" alt="QR PicPay" color="red" />
             </a>
           </div>
+          <div className="forma" id="paypal">
+            {/* <h1><img src="imgs/logo-picpay.png" alt="PicPay" /></h1>
+            <a href="http://app.picpay.com/payment?type=store&sellerId=149943" target="_blank" rel="noopener noreferrer">Clique aqui ou escaneie o QR abaixo</a>
+            <a href="http://app.picpay.com/payment?type=store&sellerId=149943" target="_blank" rel="noopener noreferrer">
+              <img src="imgs/qr-picpay-dark.png" alt="QR PicPay" color="red" />
+            </a> */}
+            {/* <h1><img src="imgs/logo-paypal.png" alt="Paypal" /></h1> */}
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+              <input type="hidden" name="cmd" value="_s-xclick" />
+              <input type="hidden" name="hosted_button_id" value="4LPY8X4XLCS4L" />
+              <input type="image" src="imgs/logo-paypal.png" name="submit" alt="Doe com Paypal" />
+              <button type="submit">Clique aqui e doe com cartão de crédito ou boleto bancário</button>
+              {/* <input type="image" src="https://www.paypalobjects.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Faça doações com o botão do PayPal" /> */}
+              {/* <img alt="" border="0" src="https://www.paypal.com/pt_BR/i/scr/pixel.gif" width="1" height="1" /> */}
+            </form>
+          </div>
         </div>
       </Navigation>
     );
